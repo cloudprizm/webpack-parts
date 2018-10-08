f3699d02e43d98352f2252cdce56727e9eb9d7fd
Damian Baar <damian.baar@gmail.com>
2018-10-08 10:04:19 +0200

    Moving parts to opensource

---
+++ README.md
@@ -0,0 +1,47 @@
+# Webpack parts
+Type and null safe, composable, webpack config for typescript.
+
+## Example
+#### Basic shape
+```typescript
+  const makeConfig: ChainableConfigDefinition = 
+    config =>
+      reader.of({ ...config })
+
+  const myDefaults = {}
+  const myEnvironment: UserConfig = {
+    target: 'node',
+    dist: '...'
+  }
+
+  const myConfig = makeConfig(myDefaults).run(myEnvironment)
+```
+
+#### More advance shape
+```typescript
+const makeConfig: ChainableConfigDefinition = config =>
+  reader
+    .of({ ...config })
+    .map(umdOutput)
+    .map(setExtensions)
+    .map(setMainFields)
+    .ap(setDevTool.map(withDevTool.set))
+    .ap(setWebpackMode.map(withMode.set))
+    .ap(sassRules.map(applyRuleAtTheEnd))
+    .ap(codeRules.map(applyRuleAtTheEnd))
+    .ap(outputEnv.map(partialOutput =>
+      withOutput.modify(output => ({ ...output, ...partialOutput }))))
+    .ap(pluginsBundle.map(applyPluginAtTheEnd))
+    .chain(graphqlRule)
+```
+
+## How to use it
+It is used in conjunction with [`@hungry/webpack-parallel`](https://github.com/hungry-consulting/webpack-parallel) - dev friendly builder and watcher for webpack.
+
+## Motivation
+In world of monorepos, there could be a tons of different configurations - at some point I had low level of trust on existing solutions, and production grade configs were hard to track and understand at first sight, so I needed something more bulletproof, easy to change - something which would sound correct without too much details.
+For me it was hard to create n-variation to any occasions and be fluent with that since code was growing and I was afraid that at some point I will loose momentum, so general idea behind this is - allow to provide any change, however within context where it will be used rather than having it prepare somewhere, far away from context. This makes config more flexible and even providing any experimental configuration does not end up with total crash.
+
+## Implementation details
+Whole configuration is based upon monad `Reader` and `Lenses`. Reader give you ability to compose any chunk of information and ability to run it within some part of common configuration. Lenses allow you to override any path in immutable type safe way. 
+Previously I was using `ramda` but it was hard to keep it small and type safe - I was not satisfied by type interference. So if you are still using `ramda` in typescript world, please have a look on `fp-ts` and `monocle-ts` library, since for me it was a game changer. Cheers!
---
+++ src/parts.ts
@@ -0,0 +1,120 @@
+
+import { array } from 'fp-ts/lib/Array'
+import { Predicate } from 'fp-ts/lib/function'
+import { asks, Reader } from 'fp-ts/lib/Reader'
+import { fromTraversable, Lens } from 'monocle-ts'
+
+import { $PropertyType } from 'utility-types'
+import { Configuration as WebpackConfig, Module, Plugin, Resolve, Rule, RuleSetRule } from 'webpack'
+import { Configuration as DevServerConfiguration } from 'webpack-dev-server'
+
+export type Entry = $PropertyType<WebpackConfig, 'entry'>
+export type Externals = $PropertyType<WebpackConfig, 'externals'>
+export type Targets = $PropertyType<WebpackConfig, 'target'>
+export type Modes = NonNullable<$PropertyType<WebpackConfig, 'mode'>>
+export type DevTool = $PropertyType<WebpackConfig, 'devtool'>
+export type Output = $PropertyType<WebpackConfig, 'output'>
+export type Optimization = $PropertyType<WebpackConfig, 'optimization'>
+export type Rules = $PropertyType<Module, 'rules'>
+export type Alias = $PropertyType<Resolve, 'alias'>
+export type Extensions = $PropertyType<Resolve, 'extensions'>
+export type MainFields = $PropertyType<Resolve, 'mainFields'>
+export type RuleType = $PropertyType<Rule, 'type'>
+
+export interface UserConfig {
+  externals: Externals
+  target: Targets,
+  env: Modes
+  dist: string
+  port: number
+  publicPath: string
+  watch: boolean
+}
+
+export type Configuration = WebpackConfig & { devServer?: DevServerConfiguration }
+export type ConfigDefinition = Reader<Partial<UserConfig>, Configuration>
+export type ChainableConfigDefinition = (config: Configuration) => ConfigDefinition
+export type ConfigPart<A> = Reader<Partial<UserConfig>, A>
+export type ChainableConfigPart = (config: Configuration) => ConfigDefinition
+
+export const module = Lens.fromNullableProp<Configuration, Module, 'module'>('module', { rules: [] })
+export const target = Lens.fromNullableProp<Configuration, Targets, 'target'>('target', 'web')
+export const mode = Lens.fromNullableProp<Configuration, Modes, 'mode'>('mode', 'development')
+export const devTool = Lens.fromNullableProp<Configuration, DevTool, 'devtool'>('devtool', 'cheap-eval-source-map')
+
+export const externals = Lens.fromNullableProp<Configuration, Externals, 'externals'>('externals', {})
+export const output = Lens.fromNullableProp<Configuration, Output, 'output'>('output', {})
+export const resolve = Lens.fromNullableProp<Configuration, Resolve, 'resolve'>('resolve', {})
+export const entry = Lens.fromNullableProp<Configuration, Entry, 'entry'>('entry', {})
+export const optimization = Lens.fromNullableProp<Configuration, Optimization, 'optimization'>('optimization', {})
+
+export const alias = Lens.fromNullableProp<Resolve, Alias, 'alias'>('alias', {})
+export const extensions = Lens.fromNullableProp<Resolve, Extensions, 'extensions'>('extensions', [])
+export const mainFields = Lens.fromNullableProp<Resolve, MainFields, 'mainFields'>('mainFields', [])
+
+export const devServer = Lens.fromNullableProp<Configuration, DevServerConfiguration, 'devServer'>('devServer', {})
+export const plugins = Lens.fromNullableProp<Configuration, Plugin[], 'plugins'>('plugins', [])
+export const rules = Lens.fromNullableProp<Module, Rules, 'rules'>('rules', [])
+
+// shorthands
+// modifiers
+export const withRules = module.compose(rules)
+export const withExternals = externals
+export const withOutput = output
+export const withPlugins = plugins
+export const withEntry = entry
+export const withOptimization = optimization
+export const withAlias = resolve.compose(alias)
+
+// overridings
+export const withTarget = target
+export const withMode = mode
+export const withDevTool = devTool
+export const withExtensions = resolve.compose(extensions)
+export const withMainFields = resolve.compose(mainFields)
+
+// getters
+export const getMode = mode
+
+export const attachDevServer: ConfigDefinition = asks(env => ({
+  contentBase: env.dist,
+  historyApiFallback: true,
+  publicPath: env.publicPath,
+  compress: true,
+  hotOnly: true,
+  port: env.port,
+  watchOptions: {
+    poll: true
+  }
+}))
+
+// predicates
+type CheckUserConfig = Predicate<Partial<UserConfig>>
+
+export const isLocal: CheckUserConfig = (userConfig) => !userConfig.env || userConfig.env === 'none'
+export const isProduction: CheckUserConfig = (userConfig) => userConfig.env === 'production'
+export const isDevelopment: CheckUserConfig = (userConfig) => userConfig.env === 'development'
+export const isWeb: CheckUserConfig = (userConfig) => userConfig.target === 'web'
+export const isNode: CheckUserConfig = (userConfig) => userConfig.target === 'node'
+
+export { RuleSetRule, RuleSetLoader, Plugin, Module } from 'webpack'
+
+// extra type
+export type LoaderRule = ConfigPart<Rule>
+export type PluginsRule = ConfigPart<Plugin[]>
+
+export const applyRuleAtTheEnd = (rule: Rule) => withRules.modify(existing => [...existing, rule])
+export const applyRuleAtTheBeginning = (rule: Rule) => withRules.modify(existing => [rule, ...existing])
+
+export const applyPluginAtTheEnd = (plugins: Plugin[]) => withPlugins.modify(existing => existing.concat(plugins))
+export const applyPluginAtTheBeginning = (plugins: Plugin[]) => withPlugins.modify(existing => plugins.concat(existing))
+
+export const ruleSetUse = Lens.fromProp<RuleSetRule, 'use'>('use')
+
+export const rulesTraversal = fromTraversable(array)<RuleSetRule>()
+export const traverseModuleRules = withRules.composeTraversal(rulesTraversal)
+
+// need to think about mappend / mempty to make genralization here -> check array/object from fp-ts
+// type ConfigurationLens<A> = Lens<Configuration, A>
+// export const extendObject = <K extends object>(entity: K) => (lens: ConfigurationLens<K>) =>
+//   lens.modify(existing => ({ ...existing, ...entity }))
